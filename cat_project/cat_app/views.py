73a2776d84cef96470c9214673c356fa7f05c34c
Muhammad Ibrahim <2890081I@student.gla.ac.uk>
2024-03-22 06:58:00 +0000

    amended the templates to be data driven by the database updated views to pass data into the templates

--- cat_project/cat_app/views.py
+++ cat_project/cat_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cat_app.models import Student, Pet
+
 
 def index(request):
+    student_list = Student.objects.order_by('last_name')
+    pet_list = Pet.objects.order_by('name')
 
-    context_dict = {'boldmessage': '[List of students and their cats go here]'}
+    context_dict = {}
+    context_dict['students'] = student_list
+
 
     return render(request, 'cat_app/index.html', context=context_dict)
 
 def pets(request):
+    pet_list = Pet.objects.order_by('name')
 
-    context_dict = {'boldmessage': '[List of all cats go here]'}
+    context_dict = {}
+    context_dict['pets'] = pet_list
 
     return render(request, 'cat_app/pets.html', context=context_dict)
